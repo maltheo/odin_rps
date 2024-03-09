d6d4139c83a7bf1aaf9b0d4cd42d2f3fc30755d8
maltheo <47742548+maltheo@users.noreply.github.com>
2024-03-09 16:15:19 +0000

    Initial functional commit
    Add basic index page and .js code that allows a game to be played.

---
+++ js/functions.js
@@ -0,0 +1,53 @@
+// use getComputerChoice to get any one of 'rock', 'paper' or 'scissors'
+
+// we could use an array to store all the options
+
+// let's start by using a random integer between 0 and 2 that returns a corresponding result
+
+// if 0, return rock, 1 returns paper and 2 returns scissors
+function getComputerChoice() {
+    // define and get a random integer in [0, 2]
+    let random_integer = Math.floor(Math.random() * 3);
+    let computerSelection;
+    switch (random_integer) {
+        case 0:
+            computerSelection = 'rock';
+            break;
+        case 1:
+            computerSelection = 'paper';
+            break;
+        case 2:
+            computerSelection = 'scissors';
+            break;
+    }
+
+    return computerSelection;
+}
+
+function getPlayerChoice() {
+    // request an input
+    let playerSelection = prompt("Enter your choice of 'rock', 'paper' or 'scissors'.", 'rock');
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    if (playerSelection != 'rock' &&
+        playerSelection != 'paper' &&
+        playerSelection != 'scissors') {
+            console.log("Invalid choice, reverting to default of 'rock'.");
+            playerSelection = 'rock';
+        }
+    return playerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        console.log("It's a tie!");
+    } else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+               (playerSelection == 'paper' && computerSelection == 'rock') ||
+               (playerSelection == 'scissors' && computerSelection == 'paper')) {
+        console.log(`You selected ${playerSelection} and your opponent selected ${computerSelection}; you win!`);
+    } else {
+        console.log(`You selected ${playerSelection} and your opponent selected ${computerSelection}; you lose!`);
+    }
+}
+
+playRound(getPlayerChoice(), getComputerChoice());
